8c689e02af0ae1c687738604424403b8ccbad49d
jimlee <jim19911126@gmail.com>
2025-04-25 15:54:31 +0800

    初步建置

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+define("PI", 19911126);
+
+$age=33;
+$name="佶璋";
+$ename="Jim";
+
+echo '我今年 ' . $age . ' 歲';
+echo "<br>";
+echo PI;
+
+echo "<hr>";
+
+echo '我是 ' . $name;
+echo "<br>";
+
+
+
